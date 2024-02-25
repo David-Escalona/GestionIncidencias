0a1cbf53d3e4c7a5f1ac901766f455e9dce54ce3
David-Escalona <escalonagarciadavid@fpllefia.com>
2024-02-25 17:25:58 +0100

    Base de datos

---
+++ bd/bd.js
@@ -0,0 +1,17 @@
+export const comentarioBD = [
+    {
+      autor: 'Messi',
+      fecha: '1/1/2023',
+      comentario: 'Comentario 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+    },
+    {
+      autor: 'Neymar',
+      fecha: '1/1/2023',
+      comentario: 'Comentario 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+    },
+    {
+      autor: 'Cristiano',
+      fecha: '1/1/2023',
+      comentario: 'Comentario 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis delectus saepe, animi ratione voluptates doloribus sint harum amet minus eius perspiciatis! Ea minima quos alias at laboriosam dolorum numquam!'
+    }
+  ]
