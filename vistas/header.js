4752d457979bb8cc8e4fc8466021ffdbae6d30cb
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-27 13:43:55 +0000

    Tasca 5. Component Header

---
+++ vistas/header.js
@@ -0,0 +1,20 @@
+export const header = {
+    template: //html
+    `
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand">Gestión de incidencias FPLLEFIA</a>
+        <div>
+          <button id="botonPanel" class="btn btn-secondary ms-2">PANEL</button>
+          <button id="botonLogin" class="btn btn-secondary ms-2">LOGIN</button>
+          <button id="botonRegistro" class="btn btn-secondary ms-2">REGISTRO</button>
+        </div>
+        <div>
+          <span>administrador@fpllefia.com</span>
+        </div>
+      </div>
+    </nav>`,
+    
+    script: () => {
+    }
+}
