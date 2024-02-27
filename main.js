403303854962a4c53cfd938635398ba66a48c5b8
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2024-02-27 16:08:06 +0000

    Commit

--- main.js
+++ main.js
@@ -1,44 +1,9 @@
-import { panel } from "./vistas/panel.js";
-import { registro } from "./vistas/registro.js";
-import { login } from "./vistas/login.js";
-import { header } from "./vistas/header.js";
+import { header } from "./componentes/header";
+import { login } from "./vistas/login";
+import { panel } from "./vistas/panel";
+import { registre } from "./vistas/registre";
 
 document.querySelector('header').innerHTML = header.template
+document.querySelector('main').innerHTML = registre.template
 header.script()
-document.querySelector('main').innerHTML = panel.template
 panel.script()
-
-const botonRegistro = document.querySelector("#botonRegistro");
-botonRegistro.addEventListener("click", cargarRegistro);
-
-function cargarRegistro(){
-  event.preventDefault();
-  document.querySelector('main').innerHTML = registro.template
-  registro.script()
-  document.getElementById("botonLogin").className = " btn btn-secondary ms-2";
-  document.getElementById("botonRegistro").className = "d-none";
-  document.getElementById("botonPanel").className = "btn btn-secondary ms-2";
-}
-
-const botonLogin = document.querySelector("#botonLogin");
-botonLogin.addEventListener("click", cargarLogin);
-
-function cargarLogin(){
-  event.preventDefault();
-  document.querySelector('main').innerHTML = login.template
-  login.script()
-  document.getElementById("botonLogin").className = "d-none";
-  document.getElementById("botonRegistro").className = "btn btn-secondary ms-2";
-  document.getElementById("botonPanel").className = "btn btn-secondary ms-2";
-}
-const botonPanel = document.querySelector("#botonPanel");
-botonPanel.addEventListener("click", cargarPanel);
-document.getElementById("botonPanel").className = "d-none";
-function cargarPanel(){
-  event.preventDefault();
-  document.querySelector('main').innerHTML = panel.template
-  panel.script()
-  document.getElementById("botonLogin").className = " btn btn-secondary ms-2";
-  document.getElementById("botonRegistro").className = "btn btn-secondary ms-2";
-  document.getElementById("botonPanel").className = "d-none";
-}
